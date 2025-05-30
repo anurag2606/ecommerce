3e5a1b0a85b953c3ca923d28ff5902aadb642636
Tushar Srivastava <tusharsri90@gmail.com>
2025-05-30 20:57:10 +0530

    improved code

--- e-commerce/src/main/java/com/codewithanurag/ecommerce/controller/PurchaseItemController.java
+++ e-commerce/src/main/java/com/codewithanurag/ecommerce/controller/PurchaseItemController.java
@@ -24,35 +24,6 @@ public class PurchaseItemController {
 
     @GetMapping("/{checkoutId}")
 	public ResponseEntity<PurchaseItemDto> getItemById(@PathVariable Integer checkoutId){
-		done();
-		System.out.print(purchaseItemService);
 		return new ResponseEntity<>(purchaseItemService.getPurchaseItem(checkoutId), HttpStatus.OK);
-	}
-
-	private void done() {
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.hashCode();
-		purchaseItemService.hashCode();
-		purchaseItemService.hashCode();
-		purchaseItemService.hashCode();
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.hashCode();
-		purchaseItemService.hashCode();
-		purchaseItemService.hashCode();
-		purchaseItemService.hashCode();
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.getPurchaseItem(0);
-		purchaseItemService.hashCode();
-		purchaseItemService.hashCode();
-		purchaseItemService.hashCode();
-		purchaseItemService.hashCode();
 	}
 }
